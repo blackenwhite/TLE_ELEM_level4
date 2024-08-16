fc941d5dc38990b2057e1d75f2df382912c6e3e1
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2024-08-16 16:59:14 +0530

    Create sending-a-sequence-over-network.cpp

---
+++ Week3:Dp-Intermediate/sending-a-sequence-over-network.cpp
@@ -0,0 +1,155 @@
+// author : Nabajyoti
+// https://codeforces.com/contest/1741/problem/E
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int mod = 1000000007;
+// int mod = 998244353;
+int modd = 1e9 + 7;
+#define pii pair<int, int>
+#define N 303
+int inf = 1e18;
+#define ff first
+#define ss second
+
+
+int gcd(int a, int b)
+{
+    if (a < b)
+        return gcd(b, a);
+    if (b == 0)
+        return a;
+    return gcd(a % b, b);
+}
+
+int mult(int a, int b) // O(1)
+{
+    return ((a % mod) * (b % mod)) % mod;
+}
+
+int exponent(int a, int b)
+{
+    int ans = 1;
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+        {
+            ans = (ans * a) % mod;
+        }
+        a = (a * a) % mod;
+        b /= 2;
+    }
+    return ans;
+}
+
+int inverse(int a) { return exponent(a, mod - 2); }
+
+int fact[N];
+int invFact[N];
+
+void init()
+{
+    fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = mult(i, fact[i - 1]);
+    }
+
+    invFact[N - 1] = inverse(fact[N - 1]);
+    for (int i = N - 2; i >= 0; i--)
+    {
+        invFact[i] = mult(invFact[i + 1], i + 1);
+    }
+}
+
+int nCr(int n, int r)
+{
+    return mult(fact[n], mult(invFact[n - r], invFact[r]));
+}
+
+int add(int a, int b)
+{ // O(1)
+    int ans = (a + b);
+    if (ans >= mod)
+        ans -= mod;
+    if (ans < 0)
+        ans += mod;
+    return ans;
+}
+
+// int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+
+void pr(vector<int> &v)
+{
+    cout << "Printing vector---\n";
+    for (auto x : v)
+    {
+        // cout << x.ff << " "<<x.ss<<"\n";
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+/*
+
+
+*/
+
+// struct less_than_key
+// {
+//     bool operator()(const Pair &a, const Pair &b)
+//     {
+//         if(a.count == b.count) return a.word<b.word;
+//         return a.count>b.count;
+//     }
+// };
+
+int dirs[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+int n;
+vector<int>a;
+
+
+void solve(){
+    cin>>n;
+    a.resize(n+1);
+
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+
+    vector<int>dp(n+1,0);
+    dp[0]=1;
+
+    for(int i=1;i<=n;i++) {
+        if(dp[i-1] && i+a[i]<=n) {
+            dp[i+a[i]]=1;
+        }
+
+        if(i-a[i]-1>=0 && dp[i-a[i]-1]) {
+            dp[i]=1;
+        }
+    }
+
+    cout<<(dp[n]?"YES":"NO")<<"\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    // for(tt=1;tt<=t;tt++){
+    //     cin>>s;
+    //     cout<<rec(0,1,1)<<"\n";
+    // }
+    return 0;
+}
